eb19c1a8f3b719696cdf6506edee9bfc0ce9d7f6
rycka13 <71268611+rycka13@users.noreply.github.com>
2022-04-10 16:06:49 +0200

    Firebase added

--- app/app/src/main/java/com/rycka13/nutritionapp/MainActivity.java
+++ app/app/src/main/java/com/rycka13/nutritionapp/MainActivity.java
@@ -1,8 +1,20 @@
 package com.rycka13.nutritionapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        
     }
 }
